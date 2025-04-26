284e5465d24e04ca5d5fa2863eeb0ef793281f62
edwin <edwinperezmarulanda@outlook.com>
2025-04-25 21:03:53 -0500

    Creando la clase conexion, Creando el objeto e instanciando conexion

---
+++ src/main/java/Conexion.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba", "root", "3179Musica");
+            if (conn != null) {
+                System.out.println("Conexión establecida");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+        }
+        return conn;
+    }
+}
